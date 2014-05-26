faf3a0fc6134ef96a3bdc02eeb8cd26faf571dcf
snakazawa <nakazawa.tms0314@gmail.com>
2014-05-26 19:20:30 +0900

    gcc,g++の追加

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -6,15 +6,18 @@ set -o vi
 rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
 cd /etc/yum.repos.d/
 ls | grep -v CentOS | xargs sed -i -e "s/enabled=1/enabled=0/g"
 cd $HOME
 
 # Install packages
 yum -y --enablerepo=epel install httpd bash-completion vim
+yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
 /sbin/chkconfig httpd on
 /sbin/service httpd start
 source /etc/profile.d/bash_completion.sh
+PATH=$PATH:/opt/rh/devtoolset-2/root/usr/bin
 
 # Disable iptables
 /sbin/chkconfig iptables off
