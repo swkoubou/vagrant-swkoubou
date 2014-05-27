42df0140a9a1cfa54f3136c1f4b58cb25490b72b
snakazawa <nakazawa.tms0314@gmail.com>
2014-05-27 10:07:41 +0900

    Python2.7と3.4のインストール追加

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -20,6 +20,38 @@ yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
 source /etc/profile.d/bash_completion.sh
 echo PATH=$PATH:/opt/rh/devtoolset-2/root/usr/bin >> /etc/profile
 echo export PATH >> /etc/profile
+source /etc/profile
+
+# Python install
+# STEP1 install needed package
+yum -y groupinstall "Development tools"
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel
+# STEP2 install Python2.7.5
+cd
+wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tar.bz2
+tar xvf Python-2.7.5.tar.bz2
+cd Python-2.7.5
+./configure --with-threads --enable-shared --prefix=/usr/local
+make && make altinstall
+rm -f Python-2.7.5.tar.bz2
+ln -s /usr/local/lib/libpython2.7.so.1.0 /lib64/
+# STEP3 install Python3.4.1
+cd
+wget http://www.python.org/ftp/python/3.4.1/Python-3.4.1.tar.xz
+tar xvf Python-3.4.1.tar.xz
+cd Python-3.4.1
+./configure --with-threads --enable-shared --prefix=/usr/local
+make && make altinstall
+rm -f Python-3.4.1.tar.xz
+ln -s /usr/local/lib/libpython3.4m.so.1.0 /lib64/
+# STEP4 librally link
+ln -s /usr/local/lib/libpython* /usr/lib
+/sbin/ldconfig -v
+ln -s /usr/local/bin/python2.7 /usr/local/bin/python
+ln -s /usr/local/include/python2.7 /usr/include/python2.7
+ln -s /usr/local/include/python3.4 /usr/include/python3.4
+# STEP5 sudo python to python2.7
+sed -i -e "s/\(secure_path = \)\(\/sbin:\/bin:\/usr\/sbin:\/usr\/bin\)/\1\/usr\/local\/bin:\2/g" /etc/sudoers
 
 # Disable iptables
 /sbin/chkconfig iptables off
